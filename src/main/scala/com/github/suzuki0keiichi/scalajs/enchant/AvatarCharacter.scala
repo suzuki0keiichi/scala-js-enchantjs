42a339c0ee42185c620fb9bd3ea334c99bbdb065
suzuki0keiichi <suzuki0keiichi@gmail.com>
2014-04-29 02:36:11 +0900

    plugins/avatarを追加

---
+++ src/main/scala/com/github/suzuki0keiichi/scalajs/enchant/AvatarCharacter.scala
@@ -0,0 +1,14 @@
+package com.github.suzuki0keiichi.scalajs.enchant.avatar
+
+import com.github.suzuki0keiichi.scalajs.enchant._
+import scala.scalajs.js.annotation.JSName
+import scala.scalajs.js
+
+@JSName("enchant.avatar.AvatarCharacter")
+class AvatarCharacter() extends Sprite(???, ???) {
+  var action: String = ???
+
+  def left() = sys.error("stub")
+
+  def right() = sys.error("stub")
+}
---
+++ src/main/scala/com/github/suzuki0keiichi/scalajs/enchant/MixingRecipe.scala
@@ -0,0 +1,18 @@
+package com.github.suzuki0keiichi.scalajs.enchant._class
+
+import com.github.suzuki0keiichi.scalajs.enchant._
+import scala.scalajs.js.annotation.JSName
+import scala.scalajs.js
+
+@JSName("enchant.Class.MixingRecipe")
+object MixingRecipe extends js.Object {
+
+  def createFromClass(sourceClass: js.Function0[js.Object]): _class.MixingRecipe = sys.error("stub")
+}
+
+@JSName("enchant.Class.MixingRecipe")
+class MixingRecipe(_decorateMethods: js.Object, _overrideMethods: js.Object, _properties: js.Object) extends Object {
+  var decorateMethods: js.Object = ???
+  var overrideMethods: js.Object = ???
+  var overrideProperties: js.Object = ???
+}
